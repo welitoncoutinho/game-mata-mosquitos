9553e5db56df82357d13590e13724778e2a99df5
welitonCoutinho <welitoncotinho@gmail.com>
2024-01-15 14:30:51 -0300

    alterei o local dos elementos para teste

--- script/script.js
+++ script/script.js
@@ -1,16 +1,20 @@
 var altura = 0
 var largura = 0
-var vidas = 1
 var tempo = 15
+var vidas = 1
+
+var mortos = 0
 
 
-var criaMosquitoTempo = 1500
+var criaMosquitoTempo = 2000
 
 var nivel = window.location.search;
 
 nivel = nivel.replace('?', '')
+if(nivel === 'facil'){
+    criaMosquitoTempo = 2000
 
-if(nivel === 'normal'){
+}else if(nivel === 'normal'){
 
     criaMosquitoTempo = 1500
 }else if(nivel === 'dificil'){
@@ -36,7 +40,7 @@ var cronometro = setInterval(function(){
     if(tempo < 0){
         clearInterval(cronometro)
         clearInterval(criaMosca)
-        window.location.href = 'vitoria.html'
+        window.location.href = "vitoria.html?" + nivel
         
     }else{
         document.getElementById('cronometro').innerHTML = tempo
@@ -50,9 +54,9 @@ function posicaoRandomica(){
         document.getElementById('mosquito').remove()
 
         if(vidas >= 3){
-            window.location.href = 'fim_de_jogo.html'
+            window.location.href = "fim_de_jogo.html?" + nivel
         }else{
-            document.getElementById('v'+vidas).src = '../imagens/coracao_vazio.png'
+            document.getElementById('v'+vidas).src = "imagens/coracao_vazio.png"
             vidas++
         }
     }
@@ -69,7 +73,7 @@ function posicaoRandomica(){
 
     //criando o elemento html
     var mosquito = document.createElement('img')
-    mosquito.src = '../imagens/mosquito.png'
+    mosquito.src = "imagens/mosquito.png"
     mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY +'px'
@@ -79,6 +83,9 @@ function posicaoRandomica(){
     
     mosquito.onclick = function(){
         this.remove()
+        mortos++
+        document.getElementById('mortos').innerHTML = mortos
+        criaMosquitoTempo = criaMosquitoTempo - 150
     }
     
 }
